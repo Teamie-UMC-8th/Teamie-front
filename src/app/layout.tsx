1dd0290e9ea75ca4c50a359df73cd73a88eaccdd
KTAEHWA <tthwa23@gmail.com>
2025-07-05 01:05:46 +0900

    🎨 Style: Pretendard 폰트 추가 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
+import localFont from 'next/font/local';
 import '../styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.className}>
       <body className="bg-white">
         <Navbar />
         <div className="flex">
