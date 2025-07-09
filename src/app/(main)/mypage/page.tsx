46d8978cf2e09222696edf23130f43970f16655d
HyunwooDoo <engusdn0421@gmail.com>
2025-07-09 17:36:31 +0900

    ✨ Feat: 지원맞춤 드롭다운 구현

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -7,7 +7,7 @@ export default function CalendarPage() {
     <div className="flex flex-col items-center h-screen py-20">
       <h1 className="text-2xl font-bold mb-4">마이페이지</h1>
       <p className="text-gray-600">이런 식으로 페이지를 작성하시면 됩니다!</p>
-      <Link href="/mypage/aiportfolio">
+      <Link href="/mypage/tailoredportfolio">
         <button className="mt-10  border border-[#BBBBBB]">AI포트폴리오</button>
       </Link>
     </div>
