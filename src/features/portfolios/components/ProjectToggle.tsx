64db212f7c17437353b547fa54adad77ec6c6431
HyunwooDoo <engusdn0421@gmail.com>
2025-07-11 21:44:29 +0900

    ⚡️ Feat: 프로젝트 추가 버튼 구현

--- src/features/portfolios/components/ProjectToggle.tsx
+++ src/features/portfolios/components/ProjectToggle.tsx
@@ -9,7 +9,7 @@ interface ProjectToggleProps {
 
 export default function ProjectToggle({ selected, setSelected }: ProjectToggleProps) {
   return (
-    <div className="flex bg-[#F8F8F8] w-[207px] h-[42px] rounded-[4px] items-center justify-center border border-[#E7E7E7]">
+    <div className="flex bg-[#F8F8F8] w-[208px] h-[42px] rounded-[4px] items-center justify-center border border-[#E7E7E7]">
       <button
         onClick={() => setSelected('project')}
         className={`w-[87px] h-[34px] text-[18px] rounded-[4px] transition-colors cursor-pointer ${
