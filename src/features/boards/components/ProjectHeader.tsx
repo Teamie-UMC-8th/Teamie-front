ee7c01ba762317ff59357bfe4d4be75d61edaf3b
KTAEHWA <tthwa23@gmail.com>
2025-07-14 22:13:10 +0900

    ✨ Feat: 나의 업무 UI 구현

---
+++ src/features/boards/components/ProjectHeader.tsx
@@ -0,0 +1,19 @@
+interface ProjectHeaderProps {
+  projectName: string;
+  isOpen: boolean;
+  onToggle: () => void;
+}
+
+export default function ProjectHeader({ projectName, isOpen, onToggle }: ProjectHeaderProps) {
+  return (
+    <button onClick={onToggle} className="cursor-pointer">
+      <div className="flex bg-[#DAF3F3] w-[325px] h-[68px] items-center justify-between rounded-[8px]">
+        <span className="font-medium text-[18px] mx-auto">{projectName}</span>
+        <img
+          src="/icons/arrow-down.svg"
+          className={`w-[32px] h-[32px] mr-[4px] ${isOpen ? 'rotate-180' : ''}`}
+        />
+      </div>
+    </button>
+  );
+}
