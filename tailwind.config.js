1840fc1361d2a022ebf1ea30c109fce738901463
Lee Yelynn <sophialee0213@gmail.com>
2025-07-18 22:35:16 +0900

    ✨ Feat: textarea 50000자로 글자 제한

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/app//.{js,ts,jsx,tsx}', './src/components/**/.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      tablet: { max: '1024px' }, // 1024px 이하일 때만 적용되는 커스텀 브레이크포인트
+    },
   },
   plugins: [],
 };
