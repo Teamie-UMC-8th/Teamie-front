aa349afde30ba9f0df0f92204c10749fc676b7ed
Lee Yelynn <sophialee0213@gmail.com>
2025-07-19 00:50:20 +0900

    🎨 Style : create 폴더 구조 분리/ai 먼저 나오게

--- src/app/(main)/mypage/aimasterportfolio/page.tsx
+++ src/app/(main)/mypage/aimasterportfolio/page.tsx
@@ -259,41 +259,7 @@ export default function AIMasterPortfolioPage() {
           />
 
           {generationMethod === 'manual' && <ManualWriteSection />}
-          {generationMethod === 'ai' && (
-            <div className="h-[804px] rounded-[16px] bg-[#F8F8F8] shadow-[0_0_8px_rgba(0,0,0,0.25)] p-[40px] flex flex-col items-start gap-[32px]">
-              {/* 상세 정보 */}
-              <div className="flex w-full">
-                <div className="w-full flex-[0.6] h-[25px] px-[18px] py-[6px] text-left font-[Pretendard] font-semibold text-[18px] leading-[25.2px] text-[#000000] whitespace-nowrap">
-                  상세 정보
-                </div>
-                <div className="w-full flex-[9.4] h-[162px] bg-[#FFFFFF] border-[1.5px] border-[#BBBBBB] rounded-[8px] font-[Pretendard] text-[18px] font-normal leading-[26px] text-black p-4" />
-              </div>
-
-              {/* 담당 업무 */}
-              <div className="flex w-full">
-                <div className="w-full flex-[0.6] h-[25px] px-[18px] py-[6px] text-left font-[Pretendard] font-semibold text-[18px] leading-[25.2px] text-[#000000] whitespace-nowrap">
-                  담당 업무
-                </div>
-                <div className="w-full flex-[9.4] h-[162px] bg-[#FFFFFF] border-[1.5px] border-[#BBBBBB] rounded-[8px] font-[Pretendard] text-[18px] font-normal leading-[26px] text-black p-4" />
-              </div>
-
-              {/* 주요 성과 */}
-              <div className="flex w-full">
-                <div className="w-full flex-[0.6] h-[25px] px-[18px] py-[6px] text-left font-[Pretendard] font-semibold text-[18px] leading-[25.2px] text-[#000000] whitespace-nowrap">
-                  주요 성과
-                </div>
-                <div className="w-full flex-[9.4] h-[162px] bg-[#FFFFFF] border-[1.5px] border-[#BBBBBB] rounded-[8px] font-[Pretendard] text-[18px] font-normal leading-[26px] text-black p-4" />
-              </div>
-
-              {/* 배운 점 */}
-              <div className="flex w-full">
-                <div className="w-full flex-[0.6] h-[25px] px-[18px] py-[6px] text-left font-[Pretendard] font-semibold text-[18px] leading-[25.2px] text-[#000000] whitespace-nowrap">
-                  배운 점
-                </div>
-                <div className="w-full flex-[9.4] h-[162px] bg-[#FFFFFF] border-[1.5px] border-[#BBBBBB] rounded-[8px] font-[Pretendard] text-[18px] font-normal leading-[26px] text-black p-4" />
-              </div>
-            </div>
-          )}
+          {generationMethod === 'ai' && <AIGenerationSetion />}
         </div>
       </div>
     </main>
