c850f50fa8ed1ac3c6332d57cd94e0cbe9108051
HyunwooDoo <engusdn0421@gmail.com>
2025-07-18 21:28:22 +0900

    🎨 Style: 반응형 테스트

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      tablet: { max: '1024px' }, // 1024px 이하일 때만 적용
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
