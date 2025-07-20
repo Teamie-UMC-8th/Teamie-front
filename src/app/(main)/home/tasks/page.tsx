6aec2e69fa8e809c45518358b18e0dbd595b8b64
KTAEHWA <tthwa23@gmail.com>
2025-07-20 20:17:47 +0900

    🎨 Style: 단위 px에서 rem으로 변경

--- src/app/(main)/home/tasks/page.tsx
+++ src/app/(main)/home/tasks/page.tsx
@@ -5,8 +5,8 @@ import MyTaskBoard from '@/features/boards/MyTaskBoard';
 export default function TaskPage() {
   return (
     <div className="min-h-screen w-full bg-white">
-      <header className="flex items-center justify-between pb-[16px] px-[8px] border-b-[2px] border-[#E7E7E7]">
-        <h1 className="text-[24px] font-bold">나의 업무</h1>
+      <header className="flex items-center justify-between pb-[1rem] px-[0.5rem] border-b-[0.125rem] border-[#E7E7E7]">
+        <h1 className="text-[1.375rem] lg:text-[1.5rem] font-semibold lg:font-bold">나의 업무</h1>
       </header>
 
       <main>
--- src/features/boards/components/ProjectHeader.tsx
+++ src/features/boards/components/ProjectHeader.tsx
@@ -8,12 +8,12 @@ export default function ProjectHeader({ projectName, isOpen, onToggle }: Project
   return (
     <button
       onClick={onToggle}
-      className="cursor-pointer flex bg-[#DAF3F3] w-[325px] h-[68px] items-center justify-between rounded-[8px]"
+      className="cursor-pointer flex bg-[#DAF3F3] w-[20.313rem] h-[4.25rem] items-center justify-between rounded-[0.5rem]"
     >
-      <span className="font-medium text-[18px] mx-auto">{projectName}</span>
+      <span className="font-medium text-[1.125rem] mx-auto">{projectName}</span>
       <img
         src="/icons/arrow-down.svg"
-        className={`w-[32px] h-[32px] mr-[4px] ${isOpen ? 'rotate-180' : ''}`}
+        className={`w-[2rem] h-[2rem] mr-[0.25rem] ${isOpen ? 'rotate-180' : ''}`}
       />
     </button>
   );
