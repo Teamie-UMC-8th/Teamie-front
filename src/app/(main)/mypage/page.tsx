c0662bef4e2b4a54626cb0d640a45ea6269e45b1
HyunwooDoo <engusdn0421@gmail.com>
2025-07-21 00:43:03 +0900

    🎨 Style: 반응형 UI 구현

--- src/app/(main)/mypage/page.tsx
+++ src/app/(main)/mypage/page.tsx
@@ -15,22 +15,37 @@ export default function MyPage() {
   return (
     <div>
       {/* TODO: Pro로 업그레이드 시에 Navbar에 Credit 충전으로 변경 */}
-      <div className="flex flex-col ml-[2.5rem]">
+      <div
+        className="flex flex-col ml-[2.5rem]
+      max-lg:ml-[-96px]"
+      >
         <h1 className="text-black text-[24px] ml-[0.5rem]">마이페이지</h1>
         <hr className="border-[#E7E7E7] border-[1px] w-[1600px] mt-[0.5rem]" />
       </div>
 
-      <main className="mt-[60px] ml-[5rem] flex">
+      <main
+        className="mt-[60px] ml-[5rem] flex
+      max-lg:ml-[-40px] max-lg:flex-col"
+      >
         {/* 나의 프로필 */}
-        <div className="flex flex-col ">
-          <h2 className="text-[22px]">나의 프로필</h2>
+        <div className="flex flex-col">
+          <h2 className="text-[22px] font-bold">나의 프로필</h2>
           <div
-            className="bg-white w-[423px] h-[599px] mt-[40px] rounded-[12px]"
+            className="bg-white w-[423px] h-[599px] mt-[40px] rounded-[12px]
+            max-lg:ml-[100px] max-lg:w-[668px] max-lg:h-[331px] max-lg:flex"
             style={{ boxShadow: '0px 0px 10px 0px #00000033' }}
           >
-            <div className="flex flex-col items-center">
+            <div
+              className="flex flex-col items-center
+            max-lg:mr-[0px]"
+            >
               <div className="relative">
-                <img src="icons/myprofile.svg" alt="Profile" className="mt-[40px] mb-[4px]" />
+                <img
+                  src="icons/myprofile.svg"
+                  alt="Profile"
+                  className="mt-[40px] mb-[4px] w-[125px] h-[125px]
+                  max-lg:ml-[60px] max-lg:mt-[60px]"
+                />
                 <button
                   className="absolute bottom-[12px] right-[10px] cursor-pointer"
                   onClick={() => alert('사진 추가')}
@@ -38,21 +53,41 @@ export default function MyPage() {
                   <img src="icons/camera-icon.svg" alt="카메라 아이콘" />
                 </button>
               </div>
-              <div className="text-black text-[22px] font-semibold mb-[36px]">김티미</div>
+              <div
+                className="text-black text-[22px] font-semibold mb-[36px]
+              max-lg:ml-[100px] max-lg:w-[100px]"
+              >
+                김티미
+              </div>
             </div>
 
-            <div className="flex flex-col items-start ml-[60px] text-[18px]">
-              <div className="flex mb-[28px]">
+            {/* 1024이하에서만 보이는 vertical line */}
+            <div className="hidden max-lg:block border-l border-[#E7E7E7] border-[1px] h-[251px] ml-[50px] mt-[40px]" />
+
+            <div
+              className="flex flex-col items-start ml-[60px] text-[18px]
+            max-lg:mt-[74px] "
+            >
+              <div
+                className="flex mb-[28px]
+              max-lg:mb-[24px] max-lg:w-[300px]"
+              >
                 <img src="icons/UnivName.svg" alt="University" className="mr-[12px]" />
                 <div className="text-[#898989] mr-[12px]">학교:</div>
                 <div className="text-black">명지대학교</div>
               </div>
-              <div className="flex mb-[28px]">
+              <div
+                className="flex mb-[28px]
+              max-lg:mb-[24px]"
+              >
                 <img src="icons/major.svg" alt="major" className="mr-[12px]" />
                 <div className="text-[#898989] mr-[12px]">전공:</div>
                 <div className="text-black">컴퓨터공학</div>
               </div>
-              <div className="flex mb-[28px]">
+              <div
+                className="flex mb-[28px]
+              max-lg:mb-[24px]"
+              >
                 <img src="icons/email.svg" alt="email" className="mr-[12px]" />
                 <div className="text-[#898989] mr-[12px]">이메일:</div>
                 <div className="text-black">Hyunwoo@mju.ac.kr</div>
@@ -68,17 +103,24 @@ export default function MyPage() {
                 <img
                   src="icons/Upgrade-pro.svg"
                   alt="UpgradePro"
-                  className="mt-[40px] ml-[133px]"
+                  className="mt-[40px] ml-[133px]
+                  max-lg:translate-x-[-680px] max-lg:translate-y-[60px]"
                 />
               )}
               {showToggle && (
-                <div className="flex flex-col">
+                <div
+                  className="flex flex-col
+                max-lg:translate-x-[-690px] max-lg:translate-y-[70px]"
+                >
                   <img
                     src="icons/AddCredit.svg"
                     alt="Credit 충전"
-                    className="w-[112px] h-[34px] ml-[156px]"
+                    className="w-[112px] h-[34px] ml-[156px] max-lg:hidden"
                   />
-                  <div className="mt-[12px] flex ml-[122px]">
+                  <div
+                    className="mt-[12px] flex ml-[122px]
+                  max-lg:w-[200px]"
+                  >
                     <div className="text-[16px] text-black">나의 잔여 Credit:</div>
                     <img src="icons/AiIcon.svg" alt="AiIcon" className="ml-[8px] mr-[4px]" />
                     <div className="text-black text-[16px]">100</div>
@@ -90,19 +132,26 @@ export default function MyPage() {
         </div>
 
         {/* 포트폴리오 */}
-        <div className="flex flex-col ml-[143px]">
+        <div
+          className="flex flex-col ml-[143px]
+        max-lg:ml-[0px] max-lg:mt-[80px]"
+        >
           <div className="flex justify-between items-center ">
-            <h2 className="text-[22px] mb-[40px]">포트폴리오</h2>
-            <div className="flex items-center">
+            <h2 className="text-[22px] mb-[40px] font-bold">포트폴리오</h2>
+            <div className="flex items-center ">
               {/* AI 첨삭일 때만 + 버튼 표시 */}
               {showToggle && selected === 'ai' && (
                 <img
                   src="icons/AddProject.svg"
                   alt="프로젝트 추가"
-                  className="w-[38px] h-[38px] mt-[3px] mr-[10px] cursor-pointer translate-y-[-18px]"
+                  className="w-[38px] h-[38px] mt-[3px] mr-[10px] cursor-pointer translate-y-[-18px]
+                  max-lg:translate-x-[-1106px] max-lg:translate-y-[32px]"
                 />
               )}
-              <div className="translate-y-[-26px] ">
+              <div
+                className="translate-y-[-26px]
+              max-lg:translate-x-[-1356px] max-lg:translate-y-[22px]"
+              >
                 {showToggle && (
                   <ToggleButton
                     leftLabel="프로젝트"
--- src/features/portfolios/Projects.tsx
+++ src/features/portfolios/Projects.tsx
@@ -2,33 +2,51 @@
 
 import Link from 'next/link';
 
-export default function Projects() {
+export default function Tailored() {
   return (
-    <div className="flex">
+    <div
+      className="flex
+    max-lg:mt-[32px]"
+    >
       <Link href="/mypage/aimasterportfolio">
         <button
-          className="bg-[#F8F8F8] w-[465px] h-[192px] rounded-[8px] grid justify-center mr-[24px] cursor-pointer"
+          className="bg-[#F8F8F8] w-[465px] h-[192px] rounded-[8px] grid justify-center mr-[24px] cursor-pointer
+          max-lg:w-[421px] max-lg:h-[180px]"
           style={{ boxShadow: '0px 0px 4px 0px #00000033' }}
         >
-          <div className="relative bg-white w-[439px] h-[48px] rounded-[4px] border-[1px] border-[#E7E7E7] flex flex-col justify-center mt-[12px]">
-            <p className="absolute text-[18px] left-[12px]">프로젝트 명</p>
+          <div
+            className="relative bg-white w-[439px] h-[48px] rounded-[4px] border-[1px] border-[#E7E7E7] flex flex-col justify-center mt-[12px]
+          max-lg:w-[397px] max-lg:h-[40px] max-lg:ml-[22px]"
+          >
+            <p
+              className="absolute text-[18px] left-[12px]
+            max-lg:text-[16px]"
+            >
+              프로젝트 명
+            </p>
             <div className="absolute right-[8px] bg-[#CDE3C9] w-[80px] h-[32px] rounded-[4px] flex items-center justify-center">
-              <span>동아리</span>
+              <span className="text-[16px]">동아리</span>
             </div>
           </div>
 
           <div className=" w-[439px] h-[96px]">
             <div className="flex mb-[12px]">
-              <div className="text-[16px] text-[#898989] mr-[38px] ml-[12px]">기여도</div>
+              <div className="text-[16px] text-[#898989] mr-[38px] ml-[12px] max-lg:ml-[36px]">
+                기여도
+              </div>
               <div className="text-[16px] text-black mr-[24px]">63%</div>
-              <img src="icons/percent-bar.svg" alt="Percent Bar" />
+              <img src="icons/percent-bar.svg" alt="Percent Bar" className="max-lg:w-[247px]" />
             </div>
             <div className="flex mb-[12px]">
-              <div className="text-[16px] text-[#898989] mr-[20px] ml-[12px]">진행 기간</div>
+              <div className="text-[16px] text-[#898989] mr-[20px] ml-[12px] max-lg:ml-[36px]">
+                진행 기간
+              </div>
               <div className="text-[16px] text-black ">25.05~25.06</div>
             </div>
             <div className="flex">
-              <div className="text-[16px] text-[#898989] mr-[20px] ml-[12px]">주요 업무</div>
+              <div className="text-[16px] text-[#898989] mr-[20px] ml-[12px] max-lg:ml-[36px]">
+                주요 업무
+              </div>
               <div className="text-[16px] text-black ">자료조사</div>
             </div>
           </div>
@@ -36,12 +54,21 @@ export default function Projects() {
       </Link>
 
       <Link href="/mypage/aimasterportfolio">
-        <div
-          className="bg-[#F8F8F8] w-[465px] h-[192px] rounded-[8px] grid justify-center cursor-pointer"
+        <button
+          className="bg-[#F8F8F8] w-[465px] h-[192px] rounded-[8px] grid justify-center cursor-pointer
+          max-lg:w-[421px] max-lg:h-[180px]"
           style={{ boxShadow: '0px 0px 4px 0px #00000033' }}
         >
-          <div className="relative bg-white w-[439px] h-[48px] rounded-[4px] border-[1px] border-[#E7E7E7] flex flex-col justify-center mt-[12px]">
-            <p className="absolute text-[18px] left-[12px]">프로젝트 명</p>
+          <div
+            className="relative bg-white w-[439px] h-[48px] rounded-[4px] border-[1px] border-[#E7E7E7] flex flex-col justify-center mt-[12px]
+          max-lg:w-[397px] max-lg:h-[40px] max-lg:ml-[22px]"
+          >
+            <p
+              className="absolute text-[18px] left-[12px]
+            max-lg:text-[16px]"
+            >
+              프로젝트 명
+            </p>
             <div className="absolute right-[8px] bg-[#FBD5D5] w-[80px] h-[32px] rounded-[4px] flex items-center justify-center">
               <span>프로젝트</span>
             </div>
@@ -49,20 +76,32 @@ export default function Projects() {
 
           <div className=" w-[439px] h-[96px]">
             <div className="flex mb-[12px]">
-              <div className="text-[16px] text-[#898989] mr-[38px] ml-[12px]">기여도</div>
+              <div className="text-[16px] text-[#898989] mr-[38px] ml-[12px] max-lg:ml-[36px]">
+                기여도
+              </div>
               <div className="text-[16px] text-black mr-[24px]">63%</div>
-              <img src="icons/percent-bar.svg" alt="Percent Bar" />
+              <img src="icons/percent-bar.svg" alt="Percent Bar" className="max-lg:w-[247px]" />
             </div>
             <div className="flex mb-[12px]">
-              <div className="text-[16px] text-[#898989] mr-[20px] ml-[12px]">진행 기간</div>
+              <div
+                className="text-[16px] text-[#898989] mr-[20px] ml-[12px] 
+                max-lg:ml-[36px]"
+              >
+                진행 기간
+              </div>
               <div className="text-[16px] text-black ">25.06~25.07</div>
             </div>
             <div className="flex">
-              <div className="text-[16px] text-[#898989] mr-[20px] ml-[12px]">주요 업무</div>
+              <div
+                className="text-[16px] text-[#898989] mr-[20px] ml-[12px] 
+              max-lg:ml-[36px]"
+              >
+                주요 업무
+              </div>
               <div className="text-[16px] text-black ">자료조사</div>
             </div>
           </div>
-        </div>
+        </button>
       </Link>
     </div>
   );
