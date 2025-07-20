480762b575f3a6ce6266c36954db9142f95713c8
KTAEHWA <tthwa23@gmail.com>
2025-07-20 20:12:24 +0900

    ✨ Feat: 나의 업무 페이지 반응형 UI 구현

--- src/app/(main)/home/tasks/page.tsx
+++ src/app/(main)/home/tasks/page.tsx
@@ -4,12 +4,12 @@ import MyTaskBoard from '@/features/boards/MyTaskBoard';
 
 export default function TaskPage() {
   return (
-    <div className="min-h-screen w-full bg-white flex flex-col">
+    <div className="min-h-screen w-full bg-white">
       <header className="flex items-center justify-between pb-[16px] px-[8px] border-b-[2px] border-[#E7E7E7]">
         <h1 className="text-[24px] font-bold">나의 업무</h1>
       </header>
 
-      <main className="flex-1 overflow-x-auto">
+      <main>
         <MyTaskBoard />
       </main>
     </div>
