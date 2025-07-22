9a2723eb75f7a5836bd7016473181dcfe8aac8e8
HyunwooDoo <engusdn0421@gmail.com>
2025-07-23 01:17:45 +0900

    ✨ Feat: 축소/구체화 토글 구현

---
+++ src/features/correction/components/ConcretizationMark.tsx
@@ -0,0 +1,10 @@
+const ConcretizationMark = () => {
+  return (
+    <div className="flex ml-[12px]">
+      <div className="bg-[#97D099] w-[4px] h-[34px] rounded-l-[4px]"></div>
+      <div className="bg-[#F5FBF5] px-[12px] py-[4px] rounded-r-[4px]"></div>
+    </div>
+  );
+};
+
+export default ConcretizationMark;
