4d343aad57c4afa273afad22eaf751089a0fc42f
HyunwooDoo <engusdn0421@gmail.com>
2025-07-23 02:57:25 +0900

    ✨ Feat: 첨삭 의뢰 시작 페이지 반응형 구현

--- src/app/(main)/mypage/addcorrection/correctionIntro/page.tsx
+++ src/app/(main)/mypage/addcorrection/correctionIntro/page.tsx
@@ -1,22 +1,51 @@
 'use client';
 
-import CorrectionStartButton from '@/features/correction/components/CorrectionStartButton';
+import CorrectionRequestStartButton from '@/features/correction/components/CorrectionRequestStartButton';
+import CorrectionStartButton from '@/features/correction/components/CorrectionRequestStartButton';
 import Link from 'next/link';
 
 export default function CorrectionIntro() {
   /* TODO: Sidebar 제거 후 간격 재조정 */
   return (
-    <div className="ml-[80px] ">
+    <div
+      className="ml-[80px]
+    max-lg:ml-[24px]"
+    >
       <div className="flex flex-col items-center">
-        <div className="w-[1323px] h-[52px] bg-[#E9F8F8] rounded-tl-[8px] rounded-tr-[8px] px-[24px] py-[12px] font-semibold text-[20px]">
+        <div
+          className="w-[1323px] h-[52px] bg-[#E9F8F8] rounded-tl-[8px] rounded-tr-[8px] px-[24px] py-[12px] font-semibold text-[20px]
+        max-lg:w-[908px]"
+        >
           AI 지원 맞춤 포트폴리오 첨삭
         </div>
-        <div className="w-[1359px] h-[800px] bg-[#F8F8F8] rounded-[16px] relative">
-          <div className="flex items-start ml-[60px] mt-[32px]">
-            <img src="/icons/AiLogo.svg" alt="AI 로고" className="translate-y-[34px]" />
-            <div className="relative ml-[28px]">
-              <img src="/icons/TitleBubble.svg" alt="말풍선" />
-              <div className="absolute top-[0px] left-[0px] text-[18px] text-black py-[44px] px-[104px] max-w-[1100px]">
+        <div
+          className="w-[1359px] h-[800px] bg-[#F8F8F8] rounded-[16px] relative
+        max-lg:w-[928px] max-lg:h-[878px]"
+        >
+          <div
+            className="flex items-start ml-[60px] mt-[32px]
+          max-lg:ml-[32px] max-lg:mt-[8px]"
+          >
+            <img
+              src="/icons/AiLogo.svg"
+              alt="AI 로고"
+              className="translate-y-[34px]
+            max-lg:w-[60px] max-lg:h-[60px] max-lg:translate-y-[32px]"
+            />
+            <div
+              className="relative ml-[28px]
+            max-lg:ml-[8px]"
+            >
+              <img src="/icons/TitleBubble.svg" alt="말풍선" className="block max-lg:hidden" />
+              <img
+                src="/icons/ResponsiveTitleBubble.svg"
+                alt="반응형 말풍선"
+                className="hidden max-lg:block"
+              />
+              <div
+                className="absolute top-[0px] left-[0px] text-[18px] text-black py-[44px] px-[104px] max-w-[1100px]
+              max-lg:px-[90px] max-lg:py-[40px]"
+              >
                 <p className="font-semibold">
                   안녕하세요! 두현우님의 포트폴리오를 첨삭할 AI, 티미입니다.
                 </p>
@@ -33,7 +62,10 @@ export default function CorrectionIntro() {
             </div>
           </div>
           <div className="relative">
-            <div className="absolute right-7 mt-[16px] ">
+            <div
+              className="absolute right-7 mt-[16px]
+            max-lg:right-0"
+            >
               <img src="/icons/SubBubble.svg" alt="말풍선" />
               <div className="absolute top-[40px] left-[50px] flex-col">
                 <p className="font-semibold text-[18px] ">기업명</p>
@@ -43,7 +75,7 @@ export default function CorrectionIntro() {
                 <p className="font-semibold text-[18px] mt-[16px]">Job Description</p>
                 <textarea className="border border-[#BBBBBB] w-[645px] h-[64px] rounded-[4px] mt-[2px] px-[12px] py-[10px] " />
 
-                <CorrectionStartButton
+                <CorrectionRequestStartButton
                   onStart={() => {
                     // 시작 로직 작성
                   }}
