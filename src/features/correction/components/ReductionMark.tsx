26bc653d02c68a1665b12b9e8bac0ebd0769bc50
HyunwooDoo <engusdn0421@gmail.com>
2025-07-23 01:17:45 +0900

    ✨ Feat: 축소/구체화 토글 구현

---
+++ src/features/correction/components/ReductionMark.tsx
@@ -0,0 +1,10 @@
+const ReductionMark = () => {
+  return (
+    <div className="flex ml-[12px]">
+      <div className="bg-[#EF7C7C] w-[4px] h-[34px] rounded-l-[4px]"></div>
+      <div className="bg-[#FDF5F5] px-[12px] py-[4px] rounded-r-[4px]"></div>
+    </div>
+  );
+};
+
+export default ReductionMark;
