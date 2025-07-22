46209bc788d450164a79f65c79f5137e77617e81
KTAEHWA <tthwa23@gmail.com>
2025-07-23 00:53:21 +0900

    ✨ Feat: 로그인 페이지 반응형 UI 구현

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -12,16 +12,20 @@ export default function LoginPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-b from-white via-white to-[#B6F5DF]">
-      <div className="flex flex-col items-start gap-[29px]">
-        <p className="text-[30px] font-bold text-black">나의 팀워크가 모이는 곳,</p>
+      <div className="flex flex-col items-start lg:gap-[29px] gap-[24px]">
+        <p className="lg:text-[30px] text-[24px] font-bold text-black">나의 팀워크가 모이는 곳,</p>
 
-        <div className="mb-[120px]">
-          <img src="/logo.svg" alt="Teamie Logo" width={545} height={80} />
+        <div className="lg:mb-[120px] mb-[80px]">
+          <img
+            src="/logo.svg"
+            alt="Teamie Logo"
+            className="lg:w-[545px] w-[409px] lg:h-[80px] h-[60px]"
+          />
         </div>
       </div>
 
       <button
-        className="self-center px-[148px] py-[12px] flex items-center gap-[8px] bg-[#FEE500] text-black rounded-[8px] font-semibold cursor-pointer"
+        className="self-center lg:px-[148px] px-[140px] lg:py-[12px] py-[8px] flex items-center gap-[8px] bg-[#FEE500] text-black rounded-[8px] lg:text-[20px] text-[18px] font-semibold cursor-pointer"
         onClick={handleLogin}
       >
         <img src="/icons/kakao.svg" alt="Kakao Logo" className="w-[22px] h-[21px]" />
