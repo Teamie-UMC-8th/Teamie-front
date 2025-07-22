d219a2175c42940f30cee4fd6200d35ad13b8b24
Lee Yelynn <sophialee0213@gmail.com>
2025-07-23 01:00:37 +0900

    ✨Feat: ai 마스터 포트폴리오 반응형 ui 구현

--- src/features/aimasterportfolio/components/StepSidebar.tsx
+++ src/features/aimasterportfolio/components/StepSidebar.tsx
@@ -17,12 +17,10 @@ export default function StepsSidebar({ steps, currentStep, goToStep }: StepSideb
   };
 
   return (
-    <div className="w-[280px]  max-lg:w-full h-full max-lg:h-[138px] bg-white border-gray-200 flex flex-col p-6
-    shadow-lg z-1">
+    <div className="w-[280px] max-lg:w-full h-full max-lg:h-[138px] bg-white border-gray-200 flex flex-col p-6 shadow-lg z-1">
       {/* 뒤로가기 */}
       <div
-        className="flex items-center gap-2 justify-end mb-8 cursor-pointer hover:bg-gray-50 p-2 rounded
-        max-lg:justify-start max-lg:mb-4"
+        className="flex items-center gap-2 justify-end mb-8 cursor-pointer hover:bg-gray-50 p-2 rounded max-lg:justify-start max-lg:mb-4"
         onClick={() => {
           if (currentStep === 0) {
             router.push('/mypage/aimasterportfolio');
@@ -43,7 +41,7 @@ export default function StepsSidebar({ steps, currentStep, goToStep }: StepSideb
           const isClickable = true;
 
           return (
-            <div key={step.id} className="relative max-lg:flex max-lg:items-center max-lg:gap-2">
+            <div key={step.id} className="relative max-lg:flex max-lg:items-center max-lg:gap-[12px]">
               {/* 스텝 항목 */}
               <div
                 className={`flex items-center gap-4 py-3 select-none transition-colors ${
@@ -60,36 +58,31 @@ export default function StepsSidebar({ steps, currentStep, goToStep }: StepSideb
                 }}
               >
                 <div
-                  className={`w-[32px] max-lg:w-[28px] h-[32px] max-lg:h-[28px] rounded-full flex items-center justify-center text-white font-medium text-lg
-                    ${isActive ? 'bg-black' : isCompleted ? 'bg-gray-400' : 'bg-gray-300'}
-                  `}
+                  className={`w-[32px] max-lg:w-[28px] h-[32px] max-lg:h-[28px] rounded-full flex items-center justify-center text-white font-medium text-lg ${
+                    isActive ? 'bg-black' : isCompleted ? 'bg-gray-400' : 'bg-gray-300'
+                  }`}
                 >
                   {step.id}
                 </div>
 
                 <span
-                  className={`
-                    text-lg font-medium max-lg:whitespace-nowrap
-                    ${isActive ? 'text-black' : isCompleted ? 'text-gray-600' : 'text-gray-500'}
-                    ${
-                      isActive
-                        ? 'max-lg:text-[18px] max-lg:leading-[26px] max-lg:font-bold max-lg:text-center max-lg:text-black'
-                        : ''
-                    }
-                  `}
+                  className={`text-lg font-medium max-lg:whitespace-nowrap ${
+                    isActive ? 'text-black' : isCompleted ? 'text-gray-600' : 'text-gray-500'
+                  } ${
+                    isActive
+                      ? 'max-lg:text-[18px] max-lg:leading-[26px] max-lg:font-bold max-lg:text-center max-lg:text-black'
+                      : ''
+                  }`}
                 >
                   {step.title}
                 </span>
               </div>
 
-
+              {/* 연결 선 */}
               {index < steps.length - 1 && (
                 <div
-                  className={`
-                    bg-gray-200
-                    absolute left-[15px] top-[44px] w-0.5 h-[44px]
-                    max-lg:static max-lg:w-[28px] max-lg:h-0.5 max-lg:left-auto max-lg:top-auto
-                  `}
+                  className={`bg-gray-200 absolute left-[15px] top-[44px] w-0.5 h-[44px] 
+                              max-lg:static max-lg:w-[28px] max-lg:h-0.5 max-lg:left-auto max-lg:top-auto`}
                 />
               )}
             </div>
