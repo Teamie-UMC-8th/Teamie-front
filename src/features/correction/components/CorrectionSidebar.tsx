f070b56224af312b05e3238d6de83d44a60cb6fa
HyunwooDoo <engusdn0421@gmail.com>
2025-07-22 18:59:36 +0900

    🎨 Style: 첨삭 페이지 사이드바 디자인 구분

--- src/features/correction/components/CorrectionSidebar.tsx
+++ src/features/correction/components/CorrectionSidebar.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import BackButton from '@/components/BackButton';
 import Link from 'next/link';
 import React from 'react';
+import { usePathname } from 'next/navigation';
 
 const CorrectionSidebar: React.FC = () => {
+  const pathname = usePathname();
+
   return (
     <aside
       className="fixed top-[0px] left-0 bg-white w-[281px] h-screen"
@@ -16,25 +18,26 @@ const CorrectionSidebar: React.FC = () => {
         </Link>
         <p className="text-[18px]">돌아가기</p>
       </div>
-      <div className="mt-[92px] ml-[40px]">
-        <button className="cursor-pointer">
-          <img src="/icons/EnterAnalyze.svg" alt="기업 정보 분석" />
-        </button>
-        <img src="/icons/ShortVLine.svg" alt="구분선" className="ml-[16px] mb-[8px] mt-[2px]" />
-        <button className="cursor-pointer">
-          <img src="/icons/ProjectSelectOff.svg" alt="프로젝트 선택" />
-        </button>
-      </div>
 
-      <div className="mt-[92px] ml-[40px]">
-        <button>
+      {/* 기업 정보 분석 */}
+      {(pathname.includes('correctionIntro') ||
+        pathname.includes('analyzing') ||
+        pathname.includes('analyzefin')) && (
+        <div className="mt-[92px] ml-[40px]">
+          <img src="/icons/EnterAnalyze.svg" alt="기업 정보 분석" />
+          <img src="/icons/ShortVLine.svg" alt="구분선" className="ml-[16px] mb-[8px] mt-[2px]" />
+          <img src="/icons/ProjectSelectOff.svg" alt="프로젝트 선택" />
+        </div>
+      )}
+
+      {/* 프로젝트 선택 */}
+      {pathname.includes('projectSelect') && (
+        <div className="mt-[92px] ml-[40px]">
           <img src="/icons/EnterAnalyzeOff.svg" alt="기업 정보 분석" />
-        </button>
-        <img src="/icons/ShortVLine.svg" alt="구분선" className="ml-[16px] mb-[8px] mt-[2px]" />
-        <button className="cursor-pointer">
+          <img src="/icons/ShortVLine.svg" alt="구분선" className="ml-[16px] mb-[8px] mt-[2px]" />
           <img src="/icons/ProjectSelect.svg" alt="프로젝트 선택" />
-        </button>
-      </div>
+        </div>
+      )}
     </aside>
   );
 };
