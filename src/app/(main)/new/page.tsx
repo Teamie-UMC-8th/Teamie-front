1bc822e08584beed7d0a04b0fa49224c533c12e2
KTAEHWA <tthwa23@gmail.com>
2025-07-23 14:41:22 +0900

    ✨ Feat: 프로젝트 생성 UI 구현

--- src/app/(main)/new/page.tsx
+++ src/app/(main)/new/page.tsx
@@ -1,8 +1,36 @@
+'use client';
+
+import { useState } from 'react';
+
 export default function New() {
+  const [inviteVisible, setInviteVisible] = useState(false);
+
   return (
-    <div className="flex flex-col items-center h-screen py-20">
-      <h1 className="text-2xl font-bold mb-4">프로젝트 생성</h1>
-      <p className="text-gray-600">이런 식으로 페이지를 작성하시면 됩니다!</p>
+    <div className="min-h-screen w-full bg-white">
+      <header className="flex items-center justify-between pb-[1rem] px-[0.5rem] border-b-[0.125rem] border-[#E7E7E7]">
+        <h1 className="text-[1.375rem] lg:text-[1.5rem] font-semibold lg:font-bold">
+          프로젝트 생성
+        </h1>
+      </header>
+
+      <main className="flex items-center justify-center mt-[128px]">
+        <div className="flex flex-col items-start justify-center gap-[16px]">
+          <div className="text-[22px] font-semibold px-[4px]">프로젝트 명</div>
+          <div className="flex justify-center gap-[16px]">
+            {/*프로젝트 이름 입력*/}
+            <input
+              placeholder="프로젝트 이름을 입력해주세요."
+              className="w-[440px] h-[50px] border-[2px] border-[#BBBBBB] rounded-[8px] px-[16px] py-[12px] text-[18px]"
+            />
+            <button
+              className="cursor-pointer self-center px-[12px] py-[4px] whitespace-nowrap bg-[#81D7D4] rounded-[4px] text-white font-bold text-[18px]"
+              onClick={() => setInviteVisible((prev) => !prev)}
+            >
+              + 생성하기
+            </button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
