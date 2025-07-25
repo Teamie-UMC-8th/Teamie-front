3e7204e18e769b44bb6be91f7989f623c10e6e17
HyunwooDoo <engusdn0421@gmail.com>
2025-07-26 01:34:19 +0900

    마이페이지 프로필 조회 API 훅 예시

---
+++ src/lib/apis/axiosInstance.ts
@@ -0,0 +1,13 @@
+// 공용 Axios 인스턴스
+
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
