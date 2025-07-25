dbf7f497b596fac27999344962b5ccd0c88eaaf0
HyunwooDoo <engusdn0421@gmail.com>
2025-07-26 01:46:27 +0900

    ✨ Feat: 공용 axiosInstance.ts 구현

---
+++ src/lib/api/axiosInstance.ts
@@ -0,0 +1,12 @@
+// 공용 axios 인스턴스
+
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
