0cdbccb73533aa11beed319f7b4b99954ff50a89
HyunwooDoo <engusdn0421@gmail.com>
2025-07-29 17:08:04 +0900

    ✨ Feat: 프로젝트 홈 기본 UI 구현

--- src/app/(main)/projects/[projectId]/page.tsx
+++ src/app/(main)/projects/[projectId]/page.tsx
@@ -2,11 +2,84 @@ import DayPicker from '@/components/DayPicker';
 
 export default function ProjectHomePage() {
   return (
-    <div className="flex flex-col items-center h-screen py-20">
-      <h1 className="text-2xl font-bold mb-4">프로젝트 홈</h1>
-      <p className="text-gray-600">이런 식으로 페이지를 작성하시면 됩니다!</p>
-      <div className="mt-8">
-        <DayPicker />
+    <div>
+      {/* 헤더 */}
+      <div className="flex flex-col">
+        <h1 className="text-[24px] text-black font-bold ml-[8px]">프로젝트 홈</h1>
+        <hr className="border border-[#E7E7E7] mt-[12px]" />
+      </div>
+      {/* 게시판, 업데이트 */}
+      <div className="flex mt-[68px] gap-[29px]">
+        <div className="flex-col">
+          <p className="text-[22px] font-semibold">게시판</p>
+          <div className="w-[920px] h-[344px] border-[2px] border-[#BBBBBB] mt-[24px] rounded-[8px] px-[48px] py-[36px] gap-x-[48px] gap-y-[32px]">
+            <img src="/icons/Post-it.svg" alt="게시판 포스트잇" className="relative" />
+          </div>
+        </div>
+        <div className="flex-col">
+          <p className="text-[22px] font-semibold">업데이트</p>
+          <div className="w-[466px] h-[344px] border-[2px] border-[#BBBBBB] mt-[24px] rounded-[8px]"></div>
+        </div>
+      </div>
+      {/* 팀 목표, 규칙 */}
+      <div className="flex mt-[80px] gap-[42px]">
+        <div>
+          <p className="text-[22px] font-semibold">우리 팀의 목표</p>
+          <textarea
+            className="w-[688px] h-[232px] border-[2px] border-[#BBBBBB] rounded-[8px] text-[20px] px-[32px] py-[20px] mt-[24px]"
+            placeholder="우리 팀의 목표를 작성하세요"
+          />
+        </div>
+        <div>
+          <div className="flex justify-between">
+            <p className="text-[22px] font-semibold">우리 팀의 규칙</p>
+            <button className="text-[18px] text-[#898989] cursor-pointer">+ 전체보기</button>
+          </div>
+          <textarea
+            className="w-[688px] h-[232px] border-[2px] border-[#BBBBBB] rounded-[8px] text-[20px] px-[32px] py-[20px] mt-[24px]"
+            placeholder="우리 팀의 규칙을 작성하세요"
+          />
+        </div>
+      </div>
+      {/* 팀원 프로필 */}
+      <div className="mt-[82px] w-[1416px]">
+        <div className="flex justify-between items-center">
+          <p className="font-semibold text-[22px]">팀원 프로필</p>
+          <button className="w-[91px] h-[34px] px-[12px] py-[4px] text-white bg-[#81D7D4] rounded-[4px] font-bold cursor-pointer">
+            팀원 추가
+          </button>
+        </div>
+        {/* 프로필 카드 */}
+        <div
+          className="w-[315px] h-[409px] rounded-[12px] bg-white mt-[24px] flex-col py-[36px] px-[40px]"
+          style={{ boxShadow: '0px 0px 10px 0px #00000033' }}
+        >
+          <img src="/icons/myprofile.svg" alt="Profile" className="w-[125px] h-[125px] ml-[55px]" />
+
+          <div className="flex items-center ml-[73px]">
+            <img src="/icons/Leader-Icon.svg" alt="리더 아이콘" />
+            <div className="font-semibold text-[22px] ml-[8px]">김티미</div>
+          </div>
+
+          <div className="w-[235px] h-[248px] flex-col mt-[18px]">
+            <div className="flex items-center py-[6px]">
+              <img src="/icons/UnivName.svg" alt="University" className="mr-[0.75rem]" />
+              <div className="text-black text-[18px]">명지대학교</div>
+            </div>
+            <div className="flex items-center py-[6px]">
+              <img src="/icons/email.svg" alt="email" className="mr-[0.75rem]" />
+              <div className="text-black text-[18px]">Hyunwoo@mju.ac.kr</div>
+            </div>
+            <div className="flex items-center py-[6px]">
+              <img src="/icons/PlanIcon.svg" alt="기획" className="mr-[0.75rem]" />
+              <div className="text-black text-[18px]">기획</div>
+            </div>
+            <div className="flex items-center py-[6px]">
+              <img src="/icons/Duties.svg" alt="담당 업무" className="mr-[0.75rem]" />
+              <div className="text-black text-[18px]">담당 업무</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
