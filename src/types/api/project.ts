703dc1d84686bb9374fb4b610ebe81bf270da2e3
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2025-07-30 18:38:42 +0900

    ✨ Feat: Project 타입 분리

--- src/types/api/project.ts
+++ src/types/api/project.ts
@@ -11,3 +11,30 @@ export interface CreateProjectReponse {
     inviteCode: string;
   };
 }
+
+export interface JoinProjectResponse {
+  isSuccess: boolean;
+  error: any;
+  result: {
+    project: {
+      id: string;
+      name: string;
+    };
+    users: Array<{
+      id: number;
+      name: string;
+      email: string;
+      school: string;
+      imageUrl: any;
+      tasks: Array<{
+        taskName: string;
+      }>;
+      permission: string;
+      role: string;
+    }>;
+    posts: Array<{
+      author: string;
+      content: string;
+    }>;
+  };
+}
