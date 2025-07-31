24ac8b704f777b833a3cd99b6e165eb0367716b4
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2025-07-31 18:21:11 +0900

    ✨ Feat: Project API 통합 및 참여 로직 변경

--- src/services/projects/createProject.ts
+++
@@ -1,9 +0,0 @@
-import axiosInstance from '@/lib/axiosInstance';
-import { CreateProjectRequest, CreateProjectReponse } from '@/types/api/project';
-
-export const postCreateProject = async (
-  body: CreateProjectRequest
-): Promise<CreateProjectReponse> => {
-  const { data } = await axiosInstance.post<CreateProjectReponse>('/api/v1/projects', body);
-  return data;
-};
--- src/services/projects/joinProject.ts
+++
@@ -1,9 +0,0 @@
-import axiosInstance from '@/lib/axiosInstance';
-import { JoinProjectResponse } from '@/types/api/project';
-
-export const getJoinProject = async (inviteCode: string): Promise<JoinProjectResponse> => {
-  const { data } = await axiosInstance.get<JoinProjectResponse>(
-    `/api/v1/projects/join?inviteCode=${inviteCode}`
-  );
-  return data;
-};
