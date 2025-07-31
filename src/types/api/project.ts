5b5e4034449a6a7049fc3922829dfd3c4e5ff53c
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2025-07-31 18:22:48 +0900

    ✨ Feat: 프로젝트 참여 요청 및 응답 타입 추가

--- src/types/api/project.ts
+++ src/types/api/project.ts
@@ -6,15 +6,26 @@ export interface CreateProjectReponse {
   isSuccess: boolean;
   error: string;
   result: {
-    id: string;
-    name: string;
-    inviteCode: string;
+    result: {
+      id: string;
+      name: string;
+      inviteCode: string;
+      expiresAt: string;
+    };
   };
 }
 
-export interface JoinProjectResponse {
+export interface GetJoinProjectRequest {
+  inviteCode: string;
+}
+
+export interface GetJoinProjectResponse {
   isSuccess: boolean;
-  error: any;
+  error: {
+    errorCode: string;
+    reason: string;
+    data: any;
+  };
   result: {
     project: {
       id: string;
@@ -36,5 +47,19 @@ export interface JoinProjectResponse {
       author: string;
       content: string;
     }>;
+  };
+}
+
+export interface PostJoinProjectRequest {
+  inviteCode: string;
+}
+
+export interface PostJoinProjectResponse {
+  isSuccess: boolean;
+  error: string | null;
+  result: {
+    message: string;
+    projectId: string;
+    projectName: string;
   };
 }
