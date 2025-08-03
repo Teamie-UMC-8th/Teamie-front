24b5817c283d5e5346a58ddf6129867b1d658041
Lee Yelynn <sophialee0213@gmail.com>
2025-08-03 22:35:04 +0900

    🎨 Design: 팀캘린더 버튼 디자인 수정

--- src/features/teamclendar/CalendarButton.tsx
+++ src/features/teamclendar/CalendarButton.tsx
@@ -8,25 +8,25 @@ export default function CalendarButton() {
   const router = useRouter();
 
   return (
-    <div className="absolute top-0 right-[20px] relative ml-auto max-lg:w-[32px] max-lg:h-[32px]">
+    <div className="relative ml-auto max-lg:w-[32px] max-lg:h-[32px]">
       {/* 메뉴 아이콘 버튼 */}
       <button onClick={() => setOpen(!open)} aria-label="메뉴 열기">
-        <img src="/icons/menu-icon.svg" alt="햄버거" className="w-[36px] h-[36px] cursor-pointer" />
+        <img src="/icons/menu-icon.svg" alt="햄버거" className=" cursor-pointer" />
       </button>
 
       {/* 드롭다운 메뉴 */}
       {open && (
         <div
-          className="absolute top-[40px] right-0 w-[265px] bg-white rounded-[8px] z-50 
+          className="absolute top-[48px] right-0 w-[265px] h-[50px] bg-white rounded-[8px] z-50 
           shadow-[0_0_15px_rgba(0,0,0,0.2)] py-[12px]"
         >
           <div className=" whitespace-nowrap flex flex-col" onClick={() => setOpen(false)}>
             <button
-              onClick={() => router.push('/mypage')} // 이거 경로 수정하면 돼요!
-              className="px-[20px] py-[4px] text-[18px] text-left leading-[26px] font-normal text-black cursor-pointer"
+              onClick={() => router.push('/project/[projectId]/teamcalendar/teamtask')} // 이거 경로 수정하면 돼요!
+              className="pl-[20px] pr-[12px] py-[4px] text-[18px] text-left leading-[26px] font-normal text-black cursor-pointer"
             >
               회의 일정 수립
-               <span className="ml-[40px] px-[10px] py-[2px] bg-[#B6F5DF] text-[#505050] text-[13px] rounded-full">
+               <span className="ml-[70px] px-[10px] py-[2px] bg-[#B6F5DF] text-[#505050] text-[13px] rounded-full">
                  진행 중
                 </span>
             </button>
