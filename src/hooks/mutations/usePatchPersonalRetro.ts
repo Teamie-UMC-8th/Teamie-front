ef07be10d48ed9f279f3886c14b577dff3b5a281
Lee Yelynn <sophialee0213@gmail.com>
2025-08-08 17:12:51 +0900

    🐛 Bug: import 배포 문제 해결

--- src/hooks/mutations/usePatchPersonalRetro.ts
+++ src/hooks/mutations/usePatchPersonalRetro.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { patchPersonalRetro } from '@/services/personalRecall/modify';
 
+
 export const usePatchPersonalRetro = () => {
   const queryClient = useQueryClient();
 
