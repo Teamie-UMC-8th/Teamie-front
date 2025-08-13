d65bbbeb71d10dd3b5493130bfa2f2ea1a74ac37
Lee Yelynn <sophialee0213@gmail.com>
2025-08-13 18:10:28 +0900

    ✨ Feat : 팀 캘린더 일정 반영 추가가

--- src/features/teamclendar/components/CalendarEventBox.tsx
+++ src/features/teamclendar/components/CalendarEventBox.tsx
@@ -1,9 +1,7 @@
-export default function CalendarEventBox({ event }: { event: { title: string } }) {
+export default function CalendarEventBox({ event }: { event: { title?: string } }) {
   return (
-    <div
-      className="bg-[#B6F5DF] w-full h-full rounded-[4px] px-[22px] py-[4px] text-sm text-black overflow-hidden whitespace-nowrap text-ellipsis"
-    >
-      {event.title}
+    <div className=" w-full h-full rounded-[4px] px-[22px] py-[4px] text-[16px] leading-[24px] text-black overflow-hidden whitespace-nowrap text-ellipsis flex items-center justify-center">
+      {event.title || '빈 일정'}
     </div>
   );
 }
