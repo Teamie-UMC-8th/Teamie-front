782797611c85d17799e73d876c289bd9e8f9bb2c
KTAEHWA <tthwa23@gmail.com>
2025-08-15 08:41:40 +0900

    ✨ Feat: 미구현 페이지 공사 티미 아이콘 추가

--- src/app/(main)/home/calendar/page.tsx
+++ src/app/(main)/home/calendar/page.tsx
@@ -1,8 +1,10 @@
 export default function CalendarPage() {
   return (
-    <div className="flex flex-col items-center h-screen py-20">
-      <h1 className="text-[24px] font-semibold">준비중이에요</h1>
-      <img src="/icons/AiCharacter.svg" alt="나의 캘린더" className="w-[150px] h-[150px]" />
+    <div className="flex flex-col items-center h-screen">
+      <div className="mt-[25vh] flex flex-col items-center">
+        <img src="/icons/constructionTeamie.svg" alt="공사중" className="w-[303px] h-[167px]" />
+        <h1 className="ml-[40px] mt-[50px] text-[22px] font-semibold">준비 중인 페이지입니다.</h1>
+      </div>
     </div>
   );
 }
