8adc6a39053cf264aebdde46669ab8e23d43f036
Lee Yelynn <sophialee0213@gmail.com>
2025-08-15 23:08:10 +0900

    🐛 Fix: 폴더명 카멜케이스 적용

--- src/hooks/mutations/usePostMasterPortfolioQuestions.ts
+++ src/hooks/mutations/usePostMasterPortfolioQuestions.ts
@@ -1,4 +1,4 @@
-import { postMasterPortfolioQuestions } from '@/services/masterportfolio/masterportfolio';
+import { postMasterPortfolioQuestions } from '@/services/masterPortfolio/masterPortfolio';
 import { useMutation } from '@tanstack/react-query';
 
 interface QuestionMutationVariables {
--- src/hooks/queries/useGetMasterPortfolio.ts
+++ src/hooks/queries/useGetMasterPortfolio.ts
@@ -5,7 +5,7 @@ import {
   getMasterPortfolioGeneratedResult,
   getMasterPortfolioStatus,
   fetchMasterPortfolioQuestions,
-} from '@/services/masterportfolio/masterportfolio';
+} from '@/services/masterPortfolio/masterPortfolio';
 import { fetchCorrectionProjects } from '@/services/correction/correction';
 import { useQuery } from '@tanstack/react-query';
 import type { MasterPortfolioDetailResponse } from '@/types/api/masterportfolio';
