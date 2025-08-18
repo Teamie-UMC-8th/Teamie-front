0791095e8da4d017d4c2f2f4eda03d9d02602a6c
KTAEHWA <tthwa23@gmail.com>
2025-08-18 23:23:58 +0900

    ✨ Feat: types/board.ts에 isCompleted 필드 추가

--- src/types/board.ts
+++ src/types/board.ts
@@ -4,9 +4,11 @@ import { StatusGroup } from '@/types/api/dashboard';
 export interface StepsBoardProps {
   steps: Step[];
   projectId: string;
+  isCompleted: boolean;
 }
 
 export interface StatusBoardProps {
   statusGroups: StatusGroup[];
   projectId: string;
+  isCompleted: boolean;
 }
