410225f2ffc84fd8ab37e345c734ea62a6e8a1f9
KTAEHWA <80040482+KTAEHWA@users.noreply.github.com>
2025-08-20 16:24:01 +0900

    🐛 Fix: 스텝명 수정 시에 클릭으로 아코디언 여닫기 가능했던 문제 해결

--- src/features/boards/components/StepHeader.tsx
+++ src/features/boards/components/StepHeader.tsx
@@ -85,6 +85,7 @@ export default function StepHeader({
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditName(e.target.value)}
               onBlur={handleBlur}
               onKeyDown={handleKeyPress}
+              onClick={(e) => e.stopPropagation()}
               className="w-full h-full text-center font-medium text-[1.125rem] bg-transparent border-none outline-none"
               autoFocus
             />
