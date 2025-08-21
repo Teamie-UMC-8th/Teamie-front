5f6c92635b91b76484aab07cb1b5878b60c569e4
KTAEHWA <tthwa23@gmail.com>
2025-08-21 22:22:19 +0900

    🐛 Fix: 클로저 문제를 해결하기 위해 onToggle 함수형 업데이트로 수정

--- src/features/aimasterportfolio/components/MenuButton.tsx
+++ src/features/aimasterportfolio/components/MenuButton.tsx
@@ -37,7 +37,7 @@ export default function MenuButton() {
     <div className="absolute top-0 right-[20px] relative ml-auto justify-end max-lg:w-[32px] max-lg:h-[32px]">
       <Dropdown
         isOpen={open}
-        onToggle={() => setOpen(!open)}
+        onToggle={() => setOpen((prev) => !prev)}
         trigger={
           <Image
             src="/icons/menu-icon.svg"
